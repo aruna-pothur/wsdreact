327fc0728e4c5bbc4746bf3aeb28c43e6763f500
aruna-pothur <157624023+aruna-pothur@users.noreply.github.com>
2024-02-06 13:50:31 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import { ThemeProvider } from '@mui/material/styles';
+import { theme } from "./styles/theme";
+
+import { BrowserRouter } from "react-router-dom";
+
+import App from './App';
+
+import { PublicClientApplication, EventType } from '@azure/msal-browser';
+
+const pca = new PublicClientApplication({
+    auth: {
+        clientId: '739d2629-c882-407e-982c-0879525ad5c8',
+        authority: 'https://login.microsoftonline.com/a3b4cbab-a447-4530-8ed7-2c77e701be99',
+        redirectUri: '/',
+    },
+    cache: {
+        cacheLocation: 'localStorage',
+        storeAuthStateInCookie: false,
+    },
+    system: {
+        loggerOptions: {
+            loggerCallback: (level, message, containsPII) => {
+                console.log(message)
+            },
+            logLevel: "Info",
+        }
+
+    }
+});
+
+pca.addEventCallback(event => {
+    if (event.eventType === EventType.LOGIN_SUCCESS) {
+        console.log(event);
+        pca.setActiveAccount(event.payload.account);
+    }
+});
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <ThemeProvider theme={theme}>
+                <App msalInstance={pca}/>
+            </ThemeProvider>
+        </BrowserRouter>
+    </React.StrictMode>
+);
